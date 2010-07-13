a90ec49f7e0bc8bb7b52ac6f93c9a2caddbe1937
Patricia Santana Cruz <patriciasc@openismus.com>
2010-07-13 17:40:16 +0200

    Set up of autotools (not completed) Changed Copirigth (C) in some documents

--- src/main.c
+++ src/main.c
@@ -1,12 +1,29 @@
-/**
-* ##############################################################################
-* # Author: Patricia Santana Cruz                                              #
-* # Version: 0.1                                                               #  
-* # Description: Game variation of Hangman                                     #
-* # Compile me with:                                                           #
-* # gcc -g main.c -o ghangtux `pkg-config --cflags --libs gtk+-2.0 gmodule-2.0`#
-* ##############################################################################
-**/
+/*
+ * ########################################################################
+ * # File: GHangtux - main.c                                              #
+ * #                                                                      #
+ * # Author: Patricia Santana Cruz  <patriciasc@openismus.com>            #
+ * #                                                                      #
+ * # Copyright (C) 2010 Openismus GmbH                                    #
+ * #                                                                      #
+ * # Version: 0.1                                                         #  
+ * #                                                                      #
+ * # Description: A variation of the Hangman game.                        #
+ * #                                                                      #
+ * # This program is free software; you can redistribute it and/or modify #
+ * # it under the terms of the GNU General Public License as published by #
+ * # the Free Software Foundation; either version 3 of the License,       #
+ * # or (at your option) any later version.                               #
+ * #                                                                      #
+ * # This program is distributed in the hope that it will be useful,      #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of       #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    #
+ * # General Public License for more details.                             #
+ * #                                                                      #
+ * # You should have received a copy of the GNU General Public License    #
+ * # along with this program. If not, see <http://www.gnu.org/licenses/>. #
+ * ########################################################################
+*/
 
 #include <gtk/gtk.h>
 #include <glib.h>
